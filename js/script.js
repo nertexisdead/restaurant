e09e2f82a43bbc5308ba992f220405e2c254be66
nertexisdead <valera_1137@mail.ru>
2023-09-22 20:16:27 +0700

    Добавил закрытие навигации при нажатии в любое место экрана

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 /* Скрипт для мобильных устройств (ширина экрана до 768px) */
 if (window.matchMedia("(max-width: 768px)").matches) {
-    console.log(123);
     document.addEventListener('DOMContentLoaded', function () {
         // Получаем ссылки на элементы
         const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
         const mainNav = document.getElementById('main-nav');
-
+    
         // Обработчик клика на кнопку меню
         mobileMenuToggle.addEventListener('click', function () {
             // Переключаем видимость меню и добавляем/удаляем класс для анимации
@@ -17,5 +16,16 @@ if (window.matchMedia("(max-width: 768px)").matches) {
                 mainNav.classList.add('slide-in');
             }
         });
+    
+        // Обработчик клика на весь документ
+        document.addEventListener('click', function (event) {
+            // Проверяем, был ли клик вне области навигации и кнопки меню
+            if (event.target !== mobileMenuToggle && event.target !== mainNav) {
+                // Скрываем навигацию
+                mainNav.style.display = 'none';
+                mainNav.classList.remove('slide-in');
+            }
+        });
     });
+    
 }
